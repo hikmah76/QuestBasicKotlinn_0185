dc1d2afad6e14d12af2eaaeb1b2a0ce6b01ba8a6
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 01:56:08 +0700

    memberikan nilai default

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -19,3 +19,16 @@ fun withNamedArgument(name: String, age: Int) {
     println("== withNamedArgument ==")
     println("Hello, $name! You are $age years old.")// Memberikan nilai default
 }
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
