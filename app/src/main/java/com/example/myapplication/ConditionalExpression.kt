a4f7988552aad64a9afaee906e8d64ecda60e174
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 01:46:09 +0700

    Try-Catch

--- app/src/main/java/com/example/myapplication/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -10,4 +10,26 @@ fun ConditionalStatement() {
     } else {
         println("Negative Number")
     }
+
+    //when
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+    //Try-Catch
+    val  value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
 }
