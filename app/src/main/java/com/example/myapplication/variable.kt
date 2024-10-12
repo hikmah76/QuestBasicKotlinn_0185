c2f791d910b89f59f6eae3c9a68152a39b142e2e
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 02:03:19 +0700

    mencetak nilai

---
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+fun main() {
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //Int
+    val intValue: Int = 30
+    println(intValue)
+}
