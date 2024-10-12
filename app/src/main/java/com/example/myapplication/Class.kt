59b64de0ee6c9c33de3c5b47888a1af0ab15a4b0
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 00:49:20 +0700

    Fungsi Kotlin

---
+++ app/src/main/java/com/example/myapplication/Class.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+
+//kode Kotlin untuk mendemonstrasikan daftar.
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
